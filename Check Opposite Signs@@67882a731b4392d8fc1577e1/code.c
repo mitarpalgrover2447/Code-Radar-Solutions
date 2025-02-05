181bcd508ecf3d6077d65ab431b996ab38b64e13
mitarpalgrover2447 <mitarpal1424.becse24@chitkara.edu.in>
2025-02-05 17:49:57 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
-
 int main() {
-    printf("%s", welcome());
+    int a,b;
+    scanf("%d %d", &a,&b);
+
+    if ((a>0 && b<0) || (a<0 && b>0)){
+        printf(
+            "True"
+        );
+    }
+    else{
+        pirntf("False");
+    }
     return 0;
 }
